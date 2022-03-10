42b6e0839113e2b2dfa63548fa462eaf7a93d25a
Jason KAWALA <j.kawala@applicam.com>
2022-03-10 16:17:45 +0100

    hover function + input grid size + ok btn listener

--- js.js
+++ js.js
@@ -1,4 +1,9 @@
 let container = document.getElementById('container');
+let reset = document.getElementById('reset-btn');
+let okBtn = document.getElementById('ok-btn');
+let grid = document.querySelectorAll('.grid-item');
+
+
 
 function createGrid(rows , cols) { /* Fonction de création de la grille de jeu */
     container.style.setProperty('--grid-rows', rows);
@@ -9,21 +14,45 @@ function createGrid(rows , cols) { /* Fonction de création de la grille de jeu 
     };
 };
 
-createGrid(64,64);
-
-
-
-
-
-
-
-
-
+createGrid(32,32);
 
 /* Event listener pour gérer le mouse over et le changement de couleur */
+function hoverListener() {
 let grid = document.querySelectorAll('.grid-item');
 grid.forEach(cell => {
     cell.addEventListener('mouseover', function() {
         cell.classList.add('hover');
     })
 });
+}
+
+hoverListener();
+
+function getVal() {
+    const val = document.querySelector('input').value;
+    console.log(val);
+    return val;
+}
+
+okBtn.addEventListener('click', () => {
+    const gridSize = getVal();
+    if (gridSize > 64 && gridSize < 2 ) {
+        window.prompt('Incorret value, please choose a grid size between 2 and 64');
+    }
+    else {
+        container.innerHTML = ""; /* Delete all child of container grid */
+        createGrid(gridSize, gridSize);
+        hoverListener();
+    }
+});
+
+/* Remove color on reset button click */
+reset.addEventListener('click', () => {
+    grid.forEach(cell => {
+        cell.classList.remove('hover');
+        console.log('reset');
+    });
+});
+
+
+

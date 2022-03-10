03df18a9c99fbdd8709a29d5242f9f3b0b8c58a2
Jason KAWALA <j.kawala@applicam.com>
2022-03-10 11:28:14 +0100

    Functional mouser over grid with color change

--- js.js
+++ js.js
@@ -1,16 +1,29 @@
 let container = document.getElementById('container');
 
-function createGrid(rows , cols) {
+function createGrid(rows , cols) { /* Fonction de création de la grille de jeu */
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div");
-        cell.innerText = (i+1);
         container.appendChild(cell).className = "grid-item";
     };
 };
-createGrid(16,16);
 
-function onHover() {
-    
-}
+createGrid(64,64);
+
+
+
+
+
+
+
+
+
+
+/* Event listener pour gérer le mouse over et le changement de couleur */
+let grid = document.querySelectorAll('.grid-item');
+grid.forEach(cell => {
+    cell.addEventListener('mouseover', function() {
+        cell.classList.add('hover');
+    })
+});

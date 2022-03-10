8bad12d81cf763ef4e505db966572388c9d3ae88
Jason KAWALA <j.kawala@applicam.com>
2022-03-10 09:40:58 +0100

    function to create grid with js finished

--- js.js
+++ js.js
@@ -7,8 +7,10 @@ function createGrid(rows , cols) {
         let cell = document.createElement("div");
         cell.innerText = (i+1);
         container.appendChild(cell).className = "grid-item";
-        console.log(i);
     };
 };
-
 createGrid(16,16);
+
+function onHover() {
+    
+}
